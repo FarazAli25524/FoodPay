05411ecf4e965f1a2ee6cd0bdae95c26ad91c97d
19SW60 <19sw60@students.muet.edu.pk>
2024-05-26 12:18:03 +0500

    FoodPay-first-commit: Creating a repository for FoodPay Application.

---
+++ src/test/java/com/foodpay/FoodPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
