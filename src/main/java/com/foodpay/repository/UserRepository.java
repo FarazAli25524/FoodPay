315fc9b0b8f7d7f849acdd522b6464208c1874f4
FPay_User_01 <farazali25524@gmail.com>
2024-06-09 19:33:25 +0500

    Commit: FPay_Commit_24.06.09
    Repository:
    i. UserRepository*

--- src/main/java/com/foodpay/repository/UserRepository.java
+++ src/main/java/com/foodpay/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,  Long> {
     public User findByEmail(String email);
-
+    public User findByEmailAndPassword(String email, String password);
 }
